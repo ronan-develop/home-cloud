87b49d60fa66daffcac9db5e8e940127f6933146
Ronan Lenouvel <ronan.develop@gmail.com>
2024-02-25 14:19:29 +0100

    bundles : symfonycasts/sass-bundle symfonycasts/tailwind-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Symfonycasts\SassBundle\SymfonycastsSassBundle::class => ['all' => true],
+    Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
 ];
