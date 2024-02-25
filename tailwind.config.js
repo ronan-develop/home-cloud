bfe0b2304a897829ea284f207c5e2fcaaf5d48cd
Ronan Lenouvel <ronan.develop@gmail.com>
2024-02-25 12:12:06 +0100

    packages : configure tailwindcss for Symfony project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./asset/**/*.js",
+    ".templates/**/*.html.twig"
+  ],
   theme: {
     extend: {},
   },
