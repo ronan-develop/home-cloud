00d4176a23ccd70c0791353ccbe30aef38b7149b
Ronan Lenouvel <ronan.develop@gmail.com>
2024-04-13 14:50:34 +0200

    config : check version

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,10 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $phpVersion = phpversion();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'phpVersion' => $phpVersion,
         ]);
     }
 }
