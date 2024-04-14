dbce938cad47b427983253218a373387fe85dd73
Ronan Lenouvel <ronan.develop@gmail.com>
2024-04-14 13:11:23 +0200

    🗹 Session : adding flash message for successful login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -17,6 +17,8 @@ class SecurityController extends AbstractController
 
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+        
+        $this->addFlash('success', 'You have successfully logged in!');
 
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
