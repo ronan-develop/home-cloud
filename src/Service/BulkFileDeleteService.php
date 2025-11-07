0aaaa784d42f95dea6ad2b4d4f4af059f8777deb
Ronan Lenouvel <ronan.lenouvel@orange.com>
2025-11-07 14:21:08 +0100

    ♻️ refact(controller): délègue la suppression en masse à BulkFileDeleteService
    Refactorise FileController pour respecter le SRP : la logique de suppression en masse est déplacée dans un service dédié.
    Facilite les tests et l’évolution métier.
    
    Files :
    - src/Controller/FileController.php
    - src/Service/BulkFileDeleteService.php
    - templates/home/_file_bulk_delete_form.html.twig

---
+++ src/Service/BulkFileDeleteService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\File;
+use App\Entity\User;
+use App\Security\FileAccessManager;
+use App\Security\FilePathSecurity;
+use Doctrine\ORM\EntityManagerInterface;
+
+class BulkFileDeleteService
+{
+    private EntityManagerInterface $em;
+    private FileAccessManager $fileAccessManager;
+    private FilePathSecurity $filePathSecurity;
+
+    public function __construct(EntityManagerInterface $em, FileAccessManager $fileAccessManager, FilePathSecurity $filePathSecurity)
+    {
+        $this->em = $em;
+        $this->fileAccessManager = $fileAccessManager;
+        $this->filePathSecurity = $filePathSecurity;
+    }
+
+    /**
+     * Supprime en masse les fichiers pour un utilisateur
+     * @param array $ids Liste des IDs de fichiers
+     * @param User $user Utilisateur courant
+     * @return int Nombre de fichiers supprimés
+     */
+    public function deleteFiles(array $ids, User $user): int
+    {
+        $count = 0;
+        foreach ($ids as $id) {
+            $file = $this->em->getRepository(File::class)->find($id);
+            if (!$file) {
+                continue;
+            }
+            try {
+                $this->fileAccessManager->assertDeleteAccess($file, $user);
+                $realPath = $this->filePathSecurity->assertSafePath($file->getPath());
+                $this->filePathSecurity->deleteFile($realPath);
+                $this->em->remove($file);
+                $count++;
+            } catch (\Throwable $e) {
+                continue;
+            }
+        }
+        $this->em->flush();
+        return $count;
+    }
+}
