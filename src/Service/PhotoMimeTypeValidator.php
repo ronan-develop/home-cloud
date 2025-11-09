84d1a8ad4609a19459cb8cd5ea25635887156c87
Ronan Lenouvel <ronan.lenouvel@orange.com>
2025-11-09 22:09:59 +0100

    ✨ feat(upload): méthode statique de validation MIME
    - Ajoute validateStatic() sur PhotoMimeTypeValidator
    - Validation ponctuelle sans instanciation, liste obligatoire en argument
    - Tests unitaires dédiés (succès et échec)
    
    Renforce la testabilité et la sécurité de la validation MIME.
    
    Closes #8 (todo méthode statique validation)

--- src/Service/PhotoMimeTypeValidator.php
+++ src/Service/PhotoMimeTypeValidator.php
@@ -7,6 +7,25 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 class PhotoMimeTypeValidator
 {
     /**
+     * Validation statique sans dépendance à l'instance.
+     * @param UploadedFile $file
+     * @param string[] $allowedMimeTypes
+     */
+    public static function validateStatic(UploadedFile $file, array $allowedMimeTypes): void
+    {
+        $mimeType = $file->getClientMimeType();
+        if (!in_array($mimeType, $allowedMimeTypes, true)) {
+            $allowed = implode(', ', $allowedMimeTypes);
+            throw new \InvalidArgumentException(
+                sprintf(
+                    "Type MIME refusé : '%s'. Types acceptés : %s",
+                    $mimeType,
+                    $allowed
+                )
+            );
+        }
+    }  
+    /**
      * @param string[] $allowedMimeTypes
      */
     public function __construct(private readonly array $allowedMimeTypes) {}
--- tests/Unit/PhotoMimeTypeValidatorTest.php
+++ tests/Unit/PhotoMimeTypeValidatorTest.php
@@ -8,6 +8,26 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class PhotoMimeTypeValidatorTest extends TestCase
 {
+    public function testStaticAcceptsValidMimeTypes(): void
+    {
+        $allowed = ['image/jpeg', 'image/png'];
+        $file = $this->createMock(UploadedFile::class);
+        $file->method('getClientMimeType')->willReturn('image/png');
+        $this->expectNotToPerformAssertions();
+        PhotoMimeTypeValidator::validateStatic($file, $allowed);
+    }
+
+    public function testStaticRejectsInvalidMimeTypes(): void
+    {
+        $allowed = ['image/jpeg', 'image/png'];
+        $file = $this->createMock(UploadedFile::class);
+        $file->method('getClientMimeType')->willReturn('application/pdf');
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            "Type MIME refusé : 'application/pdf'. Types acceptés : image/jpeg, image/png"
+        );
+        PhotoMimeTypeValidator::validateStatic($file, $allowed);
+    }
 
     public function testAcceptsValidMimeTypes(): void
     {
