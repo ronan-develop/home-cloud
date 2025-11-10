1e8cd8b6f729dd65165833a03ce9f600a678b10b
Ronan Lenouvel <ronan.lenouvel@orange.com>
2025-11-10 14:33:59 +0100

    🐛 fix(securite): accès sécurisé aux photos et gestion 404 explicite
    - Ajout du voter PhotoVoter : accès autorisé au propriétaire ou admin
    - Correction du contrôleur pour 404 explicite si photo ou fichier absent
    - Prépare la base pour la sécurisation complète de la galerie photo
    
    #securite #photo #voter #fix

--- src/Controller/PhotoServeController.php
+++ src/Controller/PhotoServeController.php
@@ -17,9 +17,15 @@ class PhotoServeController extends AbstractController
     public function view(int $id, PhotoRepository $repo): BinaryFileResponse
     {
         $photo = $repo->find($id);
+        if (!$photo) {
+            throw $this->createNotFoundException('Photo introuvable.');
+        }
         $this->denyAccessUnlessGranted('view', $photo); // Voter ou logique d'accès
 
         $file = $this->getParameter('app.photos_dir') . '/' . $photo->getFilename();
+        if (!is_file($file)) {
+            throw $this->createNotFoundException('Fichier photo manquant sur le serveur.');
+        }
         return (new BinaryFileResponse($file))
             ->setContentDisposition(ResponseHeaderBag::DISPOSITION_INLINE, $photo->getOriginalName());
     }
