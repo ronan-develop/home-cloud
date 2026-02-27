273f746af416a1f2b53c57edcae859f431710a6f
Ronan Lenouvel <ronan.lenouvel@orange.com>
2026-02-27 21:50:34 +0100

    ✨ feat(CreateUserCommand): implémente la création d'utilisateur via CLI

---
+++ src/Command/CreateUserCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(name: 'app:create-user', description: 'Crée un utilisateur')]
+class CreateUserCommand extends Command
+{
+    public function __construct(
+        private readonly EntityManagerInterface $em,
+        private readonly UserPasswordHasherInterface $hasher,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('email', InputArgument::REQUIRED, 'Email')
+            ->addArgument('password', InputArgument::REQUIRED, 'Mot de passe')
+            ->addArgument('displayName', InputArgument::OPTIONAL, 'Nom affiché', 'User');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $user = new User($input->getArgument('email'), $input->getArgument('displayName'));
+        $user->setPassword($this->hasher->hashPassword($user, $input->getArgument('password')));
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $io->success('User créé : ' . $user->getEmail());
+
+        return Command::SUCCESS;
+    }
+}
