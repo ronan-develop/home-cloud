dcbcd55041d55cd73245f769870cb0e9645d0748
Ronan Lenouvel <ronan.lenouvel@orange.com>
2026-03-01 18:57:10 +0100

    ✨ feat(FolderCard): ajout du composant FolderCard (Twig+PHP) pour carte dossier/fichier

---
+++ src/Twig/Components/FolderCard.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Twig\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent(name: 'FolderCard')]
+class FolderCard
+{
+    public object $item;
+    // Props : objet dossier/fichier (doit avoir name, isFolder, etc.)
+}
