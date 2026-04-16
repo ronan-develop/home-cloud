82e019ee4422040265e3ac6f267fa5c232270984
Ronan Lenouvel <ronan.lenouvel@orange.com>
2026-04-16 13:41:53 +0200

    🛠️ chore(stimulus): supprimer new_menu_controller et file_upload_controller (jamais appliqués)

--- assets/stimulus_bootstrap.js
+++ assets/stimulus_bootstrap.js
@@ -1,7 +1,3 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
-import NewMenuController from './controllers/new_menu_controller.js';
-import FileUploadController from './controllers/file_upload_controller.js';
 
 const app = startStimulusApp();
-app.register('new-menu', NewMenuController);
-app.register('file-upload', FileUploadController);
